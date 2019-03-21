907420d48c885d2c83c457843e02fb0a0da4382f
Joseph Lionarons <lionsamurai@gmail.com>
2019-03-21 12:54:38 -0400

    final commit before interview

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p>TripleByte application goes here!</p>
-        </header>
+        <p>TripleByte application goes here!</p>
       </div>
     );
   }
